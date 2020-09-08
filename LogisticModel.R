3c51aa9fe52837e9f2b50e1dceef1a1f5eb167d8
mb3005 <michellethuybui@gmail.com>
2020-09-07 20:49:24 -0400

    test

---
+++ LogisticModel.R
@@ -0,0 +1,19 @@
+
+library(tidyverse)
+
+#to correct forparsing failures; incorrect "logical" class for columns: bottom3, bottom4, bottom5, bottom 6
+
+RPDR <- read_csv(file.path("Data", "combine_clean.csv"),
+                        col_names = TRUE,
+                        col_types = cols(
+                              bottom3 = col_character(),
+                              bottom4 = col_character(),
+                              bottom5 = col_character(),
+                              bottom6 = col_character()
+                          ))
+class(RPDR$bottom3)
+class(RPDR$bottom4)
+class(RPDR$bottom5)
+class(RPDR$bottom6)
+
+
