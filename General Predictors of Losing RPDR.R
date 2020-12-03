31ac4e81c91e54b5a20c99a929db0538488f3256
mb3005 <michellethuybui@gmail.com>
2020-12-02 19:05:25 -0500

    Data Merge 2

--- General Predictors of Losing RPDR.R
+++ General Predictors of Losing RPDR.R
@@ -1,90 +1,149 @@
 
 library(tidyverse)
 
-#to correct for parsing failures; R was reading as "logical" class for columns: bottom3, bottom4, bottom5, bottom 6
+#to correct for parsing failures; R was reading as "logical" class many different
+# columns
 
-RPDR <- read_csv(file.path("Data", "combine.csv"),
-                        col_names = TRUE,
-                        col_types = cols(
-                              bottom3 = col_character(),
-                              bottom4 = col_character(),
-                              bottom5 = col_character(),
-                              bottom6 = col_character()
+RPDR <- read_csv(file.path("Data Files", "combined - 04112020.csv"),
+                          col_names = TRUE,
+                          col_types = cols(
+                             Lip_Sync_Number = col_factor(),
+                             Bottom3 = col_factor(),
+                             Bottom4 = col_factor(),
+                             Bottom5 = col_factor(),
+                             Bottom6 = col_factor()
                           ))
-class(RPDR$bottom3)
-class(RPDR$bottom4)
-class(RPDR$bottom5)
-class(RPDR$bottom6)
 
-Age <- glm(OUTCOME_LOSS ~ age + as.factor(Episode), family=binomial(link='logit'), data=RPDR)
-summary(age)
+RPDR <- RPDR %>%
+    mutate_all(as.factor)
 
-Hometown_City <- glm(OUTCOME_LOSS ~ hometown_city + as.factor(Episode), family=binomial(link='logit'), data=RPDR)
-summary(hometown_city)
+RPDR <- RPDR %>%
+        mutate_at(vars('Age','Numqueens', 'Numcontests', 'Perc_High', 'Perc_Win', 'Perc_Winhigh', 'Perc_Low',
+                       'Perc_Btm', 'Perc_Lowbtm', 'Num_High', 'Num_Win', 'Num_Winhigh',
+                       'Num_Btm', 'Num_Low', 'Num_Lowbtm', 'Db_Score', 'Points', 'PPE'),
+                        as.character)
 
-Hometown_State <- glm(OUTCOME_LOSS ~ hometown_state + as.factor(Episode), family=binomial(link='logit'), data=RPDR)
-summary(hometown_state)
+RPDR <- RPDR %>%
+  mutate_at(vars('Age','Numqueens', 'Numcontests', 'Perc_High', 'Perc_Win', 'Perc_Winhigh', 'Perc_Low',
+                 'Perc_Btm', 'Perc_Lowbtm', 'Num_High', 'Num_Win', 'Num_Winhigh',
+                 'Num_Btm', 'Num_Low', 'Num_Lowbtm', 'Db_Score', 'Points', 'PPE'),
+            as.numeric)
+str(RPDR)
 
-Wig_Removed <- glm(OUTCOME_LOSS ~ Wig_Removed + as.factor(Episode), family=binomial(link='logit'), data=RPDR)
-summary(Wig_Removed)
+RPDR$DOB <- as.Date(as.numeric(as.character(RPDR$DOB)),origin="1960-01-01")
+RPDR$Airdate <- as.Date(RPDR$Airdate,format="%Y-%m-%d")
 
-#getting a warning message after running glm: glm.fit: fitted probabilities numerically 0 or 1 occurred
-Death_Drop <- glm(OUTCOME_LOSS ~ Death_Drop + as.factor(Episode), family=binomial(link='logit'), data=RPDR)
-summary(Death_Drop)
+#recoding factor levels
+RPDR$Gender <- RPDR$Gender %>%
+    fct_recode("Cis" = "0", "Trans" = "1")
+levels(RPDR$Gender)
+RPDR$Gender
 
-Outfit_Reveal <- glm(OUTCOME_LOSS ~ Outfit_Reveal + as.factor(Episode), family=binomial(link='logit'), data=RPDR)
-summary(Outfit_Reveal)
+RPDR$Race <- RPDR$Race %>%
+    fct_recode('White'='0', 'Latin'='2', 'Asian'='3', 'Black'='1', 'Other'='4')
+levels(RPDR$Race)
+RPDR$Race
 
-Quality_Of_Outfit <- glm(OUTCOME_LOSS ~ Quality_of_outfit + as.factor(Episode), family=binomial(link='logit'), data=RPDR)
-summary(Quality_of_outfit)
+RPDR$Quality_Of_Outfit <- RPDR$Quality_Of_Outfit %>%
+    fct_recode('Boot'='0', 'Toot'='1', 'Supertoot'='2')
+levels(RPDR$Quality_Of_Outfit)
+RPDR$Quality_Of_Outfit
 
-Merle <- glm(OUTCOME_LOSS ~ Merle + as.factor(Episode), family=binomial(link='logit'), data=RPDR)
-summary(Merle)
+RPDR$Body_Type <- RPDR$Body_Type %>%
+    fct_recode('Not Big'='0', 'Thick n Juicy'='1', 'Chunky Yet Funky'='2')
+levels(RPDR$Body_Type)
+RPDR$Body_Type
 
-Michelle <- glm(OUTCOME_LOSS ~ Michelle + as.factor(Episode), family=binomial(link='logit'), data=RPDR)
-summary(Michelle)
+RPDR$Type_Queen <- RPDR$Type_Queen %>%
+    fct_recode('Comedy'='0', 'Pageant'='1', 'Look'='2', 'Other'='3')
+levels(RPDR$Type_Queen)
+RPDR$Type_Queen
 
-Santino <- glm(OUTCOME_LOSS ~ Santino + as.factor(Episode), family=binomial(link='logit'), data=RPDR)
-summary(Santino)
+#setting consistent reference levels
+RPDR$OUTCOME_LOSS <- relevel(RPDR$OUTCOME_LOSS, ref = "0")
 
-Ross <- glm(OUTCOME_LOSS ~ Ross + as.factor(Episode), family=binomial(link='logit'), data=RPDR)
-summary(Ross)
+RPDR$Hometown_State <- relevel(RPDR$Hometown_State, ref = "New York")
 
-Carson <- glm(OUTCOME_LOSS ~ Carson + as.factor(Episode), family=binomial(link='logit'), data=RPDR)
-summary(Carson)
+RPDR$Hometown_City <- relevel(RPDR$Hometown_City, ref = "New York")
 
-Do_They_Know_Words <- glm(OUTCOME_LOSS ~ Do_they_know_words + as.factor(Episode), family=binomial(link='logit'), data=RPDR)
-summary(Do_they_know_words)
+RPDR$Quality_of_outfit <- relevel(RPDR$Quality_Of_Outfit, ref = "Boot")
 
-Outfit_Reveal_1 <- glm(OUTCOME_LOSS ~ Outfit_Reveal_1 + as.factor(Episode), family=binomial(link='logit'), data=RPDR)
-summary(Outfit_Reveal_1)
+RPDR$Wig_Removed <- relevel(RPDR$Wig_Removed, ref = "0")
 
-Gender <- glm(OUTCOME_LOSS ~ Gender + as.factor(Episode), family=binomial(link='logit'), data=RPDR)
-summary(Gender)
+RPDR$Death_Drop <- relevel(RPDR$Death_Drop, ref = "0")
 
-Race <- glm(OUTCOME_LOSS ~ Race + as.factor(Episode), family=binomial(link='logit'), data=RPDR)
-summary(Race)
+RPDR$Outfit_Reveal <- relevel(RPDR$Outfit_Reveal, ref = "0")
 
-Body_Type <- glm(OUTCOME_LOSS ~ Body_Type + as.factor(Episode), family=binomial(link='logit'), data=RPDR)
-summary(Body_Type)
+RPDR$Merle <- relevel(RPDR$Merle, ref = "0")
 
-Type_Queen <- glm(OUTCOME_LOSS ~ Type_Queen + as.factor(Episode), family=binomial(link='logit'), data=RPDR)
-summary(Type_Queen)
+RPDR$Michelle <- relevel(RPDR$Michelle, ref = "0")
 
-Sewing <- glm(OUTCOME_LOSS ~ Sewing + as.factor(Episode), family=binomial(link='logit'), data=RPDR)
-summary(Sewing)
+RPDR$Ross <- relevel(RPDR$Ross, ref = "0")
 
-Dancing <- glm(OUTCOME_LOSS ~ Dancing + as.factor(Episode), family=binomial(link='logit'), data=RPDR)
-summary(Dancing)
+RPDR$Carson <- relevel(RPDR$Carson, ref = "0")
 
-Singing <- glm(OUTCOME_LOSS ~ Singing + as.factor(Episode), family=binomial(link='logit'), data=RPDR)
-summary(Singing)
+RPDR$Do_they_know_words <- relevel(RPDR$Do_They_Know_Words, ref = "0")
 
-Lip_Sync_Ass <- glm(OUTCOME_LOSS ~ Lip_Sync_Ass + as.factor(Episode), family=binomial(link='logit'), data=RPDR)
-summary(Lip_Sync_Ass)
+RPDR$Outfit_Reveal_1 <- relevel(RPDR$Outfit_Reveal_1, ref = "0")
 
-Expressed_Hardship <- glm(OUTCOME_LOSS ~ Expressed_Hardship + as.factor(Episode), family=binomial(link='logit'), data=RPDR)
-summary(Expressed_Hardship)
+RPDR$Sewing <- relevel(RPDR$Sewing, ref = "0")
 
+RPDR$Dancing <- relevel(RPDR$Dancing, ref = "0")
+
+RPDR$Singing <- relevel(RPDR$Singing, ref = "0")
+
+RPDR$Lip_Sync_Ass <- relevel(RPDR$Lip_Sync_Ass, ref = "0")
+
+RPDR$Expressed_Hardship <- relevel(RPDR$Expressed_Hardship, ref = "0")
+
+
+########################### UNIT BIVARIATE MODELS ###########################
+
+
+summary(Age <- glm(OUTCOME_LOSS ~ Age + as.factor(Episode), family=binomial(link='logit'), data=RPDR))
+
+summary(Hometown_City <- glm(OUTCOME_LOSS ~ Hometown_City + as.factor(Episode), family=binomial(link='logit'), data=RPDR))
+
+summary(Hometown_State <- glm(OUTCOME_LOSS ~ Hometown_State + as.factor(Episode), family=binomial(link='logit'), data=RPDR))
+
+summary(Wig_Removed <- glm(OUTCOME_LOSS ~ Wig_Removed + as.factor(Episode), family=binomial(link='logit'), data=RPDR))
+
+summary(Death_Drop <- glm(OUTCOME_LOSS ~ Death_Drop + as.factor(Episode), family=binomial(link='logit'), data=RPDR))
+
+summary(Outfit_Reveal <- glm(OUTCOME_LOSS ~ Outfit_Reveal + as.factor(Episode), family=binomial(link='logit'), data=RPDR))
+
+summary(Quality_Of_Outfit <- glm(OUTCOME_LOSS ~ Quality_of_outfit + as.factor(Episode), family=binomial(link='logit'), data=RPDR))
+
+summary(Merle <- glm(OUTCOME_LOSS ~ Merle + as.factor(Episode), family=binomial(link='logit'), data=RPDR))
+
+summary(Michelle <- glm(OUTCOME_LOSS ~ Michelle + as.factor(Episode), family=binomial(link='logit'), data=RPDR))
+
+summary(Santino <- glm(OUTCOME_LOSS ~ Santino + as.factor(Episode), family=binomial(link='logit'), data=RPDR))
+
+summary(Ross <- glm(OUTCOME_LOSS ~ Ross + as.factor(Episode), family=binomial(link='logit'), data=RPDR))
+
+summary(Carson <- glm(OUTCOME_LOSS ~ Carson + as.factor(Episode), family=binomial(link='logit'), data=RPDR))
+
+summary(Do_They_Know_Words <- glm(OUTCOME_LOSS ~ Do_They_Know_Words + as.factor(Episode), family=binomial(link='logit'), data=RPDR))
+
+summary(Outfit_Reveal_1 <- glm(OUTCOME_LOSS ~ Outfit_Reveal_1 + as.factor(Episode), family=binomial(link='logit'), data=RPDR))
+
+summary(Gender <- glm(OUTCOME_LOSS ~ Gender + as.factor(Episode), family=binomial(link='logit'), data=RPDR))
+
+summary(Race <- glm(OUTCOME_LOSS ~ Race + as.factor(Episode), family=binomial(link='logit'), data=RPDR))
+
+summary(Body_Type <- glm(OUTCOME_LOSS ~ Body_Type + as.factor(Episode), family=binomial(link='logit'), data=RPDR))
+
+summary(Type_Queen <- glm(OUTCOME_LOSS ~ Type_Queen + as.factor(Episode), family=binomial(link='logit'), data=RPDR))
+
+summary(Sewing <- glm(OUTCOME_LOSS ~ Sewing + as.factor(Episode), family=binomial(link='logit'), data=RPDR))
+
+summary(Dancing <- glm(OUTCOME_LOSS ~ Dancing + as.factor(Episode), family=binomial(link='logit'), data=RPDR))
+
+summary(Singing <- glm(OUTCOME_LOSS ~ Singing + as.factor(Episode), family=binomial(link='logit'), data=RPDR))
+
+summary(Lip_Sync_Ass <- glm(OUTCOME_LOSS ~ Lip_Sync_Ass + as.factor(Episode), family=binomial(link='logit'), data=RPDR))
+
+summary(Expressed_Hardship <- glm(OUTCOME_LOSS ~ Expressed_Hardship + as.factor(Episode), family=binomial(link='logit'), data=RPDR))
 
 
--- Predictors of Losing In Bottom Two.R
+++ Predictors of Losing In Bottom Two.R
@@ -2,86 +2,140 @@ library(tidyverse)
 
 #to correct for parsing failures; R was reading as "logical" class for columns: bottom3, bottom4, bottom5, bottom 6
 
-Bottom_Two <- read_csv(file.path("Data", "Bottom_Two_Data.csv"),
-                 col_names = TRUE,
-                 col_types = cols(
-                   bottom3 = col_character(),
-                   bottom4 = col_character(),
-                   bottom5 = col_character(),
-                   bottom6 = col_character(),
-                   minicw3 = col_character()
-                 ))
-class(Bottom_Two$bottom3)
-class(Bottom_Two$bottom4)
-class(Bottom_Two$bottom5)
-class(Bottom_Two$bottom6)
+Bottom_Two <- read_csv(file.path("Data Files", "Bottom_Two.csv"))
 
-Age <- glm(OUTCOME_LOSS ~ age, family=binomial(link='logit'), data=Bottom_Two)
-summary(Age)
+Bottom_Two <- Bottom_Two %>%
+  mutate_all(as.factor)
 
-Hometown_City <- glm(OUTCOME_LOSS ~ hometown_city, family=binomial(link='logit'), data=Bottom_Two)
-summary(Hometown_City)
+Bottom_Two <- Bottom_Two %>%
+  mutate_at(vars('Age','Numqueens', 'Numcontests', 'Perc_High', 'Perc_Win', 'Perc_Winhigh', 'Perc_Low',
+                 'Perc_Btm', 'Perc_Lowbtm', 'Num_High', 'Num_Win', 'Num_Winhigh',
+                 'Num_Btm', 'Num_Low', 'Num_Lowbtm', 'Db_Score', 'Points', 'PPE'),
+            as.character)
 
-Hometown_State <- glm(OUTCOME_LOSS ~ hometown_state, family=binomial(link='logit'), data=Bottom_Two)
-summary(Hometown_State)
 
-Wig_Removed <- glm(OUTCOME_LOSS ~ Wig_Removed, family=binomial(link='logit'), data=Bottom_Two)
-summary(Wig_Removed)
+Bottom_Two <- Bottom_Two %>%
+  mutate_at(vars('Age','Numqueens', 'Numcontests', 'Perc_High', 'Perc_Win', 'Perc_Winhigh', 'Perc_Low',
+                 'Perc_Btm', 'Perc_Lowbtm', 'Num_High', 'Num_Win', 'Num_Winhigh',
+                 'Num_Btm', 'Num_Low', 'Num_Lowbtm', 'Db_Score', 'Points', 'PPE'),
+            as.numeric)
 
-Death_Drop <- glm(OUTCOME_LOSS ~ Death_Drop, family=binomial(link='logit'), data=Bottom_Two)
-summary(Death_Drop)
+str(Bottom_Two)
 
-Outfit_Reveal <- glm(OUTCOME_LOSS ~ Outfit_Reveal, family=binomial(link='logit'), data=Bottom_Two)
-summary(Outfit_Reveal)
+Bottom_Two$DOB <- as.Date(as.numeric(as.character(Bottom_Two$DOB)),origin="1960-01-01")
+Bottom_Two$Airdate <- as.Date(Bottom_Two$Airdate,format="%Y-%m-%d")
 
-Quality_Of_Outfit <- glm(OUTCOME_LOSS ~ Quality_of_outfit, family=binomial(link='logit'), data=Bottom_Two)
-summary(Quality_Of_Outfit)
 
-Merle <- glm(OUTCOME_LOSS ~ Merle, family=binomial(link='logit'), data=Bottom_Two)
-summary(Merle)
+#recoding factor levels
 
-Michelle <- glm(OUTCOME_LOSS ~ Michelle, family=binomial(link='logit'), data=Bottom_Two)
-summary(Michelle)
+Bottom_Two$Gender <- Bottom_Two$Gender %>%
+  fct_recode("Cis" = "0", "Trans" = "1")
+levels(Bottom_Two$Gender)
+Bottom_Two$Gender
 
-Santino <- glm(OUTCOME_LOSS ~ Santino, family=binomial(link='logit'), data=Bottom_Two)
-summary(Santino)
+Bottom_Two$Race <- Bottom_Two$Race %>%
+  fct_recode('White'='0', 'Latin'='2', 'Asian'='3', 'Black'='1', 'Other'='4')
+levels(Bottom_Two$Race)
+Bottom_Two$Race
 
-Ross <- glm(OUTCOME_LOSS ~ Ross, family=binomial(link='logit'), data=Bottom_Two)
-summary(Ross)
+Bottom_Two$Quality_Of_Outfit <- Bottom_Two$Quality_Of_Outfit %>%
+  fct_recode('Boot'='0', 'Toot'='1', 'Supertoot'='2')
+levels(Bottom_Two$Quality_Of_Outfit)
+Bottom_Two$Quality_Of_Outfit
 
-Carson <- glm(OUTCOME_LOSS ~ Carson, family=binomial(link='logit'), data=Bottom_Two)
-summary(Carson)
+Bottom_Two$Body_Type <- Bottom_Two$Body_Type %>%
+  fct_recode('Not Big'='0', 'Thick n Juicy'='1', 'Chunky Yet Funky'='2')
+levels(Bottom_Two$Body_Type)
+Bottom_Two$Body_Type
 
-Do_They_Know_Words <- glm(OUTCOME_LOSS ~ Do_they_know_words, family=binomial(link='logit'), data=Bottom_Two)
-summary(Do_They_Know_Words)
+Bottom_Two$Type_Queen <- Bottom_Two$Type_Queen %>%
+  fct_recode('Comedy'='0', 'Pageant'='1', 'Look'='2', 'Other'='3')
+levels(Bottom_Two$Type_Queen)
+Bottom_Two$Type_Queen
 
-Outfit_Reveal_1 <- glm(OUTCOME_LOSS ~ Outfit_Reveal_1, family=binomial(link='logit'), data=Bottom_Two)
-summary(Outfit_Reveal_1)
+#setting consistent reference levels
 
-Gender <- glm(OUTCOME_LOSS ~ Gender, family=binomial(link='logit'), data=Bottom_Two)
-summary(Gender)
+Bottom_Two$OUTCOME_LOSS <- relevel(Bottom_Two$OUTCOME_LOSS, ref = "0")
 
-Race <- glm(OUTCOME_LOSS ~ Race, family=binomial(link='logit'), data=Bottom_Two)
-summary(Race)
+Bottom_Two$Hometown_State <- relevel(Bottom_Two$Hometown_State, ref = "New York")
 
-Body_Type <- glm(OUTCOME_LOSS ~ Body_Type, family=binomial(link='logit'), data=Bottom_Two)
-summary(Body_Type)
+Bottom_Two$Hometown_City <- relevel(Bottom_Two$Hometown_City, ref = "New York")
 
-Type_Queen <- glm(OUTCOME_LOSS ~ Type_Queen, family=binomial(link='logit'), data=Bottom_Two)
-summary(Type_Queen)
+Bottom_Two$Quality_of_outfit <- relevel(Bottom_Two$Quality_Of_Outfit, ref = "Boot")
 
-Sewing <- glm(OUTCOME_LOSS ~ Sewing, family=binomial(link='logit'), data=Bottom_Two)
-summary(Sewing)
+Bottom_Two$Wig_Removed <- relevel(Bottom_Two$Wig_Removed, ref = "0")
 
-Dancing <- glm(OUTCOME_LOSS ~ Dancing, family=binomial(link='logit'), data=Bottom_Two)
-summary(Dancing)
+Bottom_Two$Death_Drop <- relevel(Bottom_Two$Death_Drop, ref = "0")
 
-Singing <- glm(OUTCOME_LOSS ~ Singing, family=binomial(link='logit'), data=Bottom_Two)
-summary(Singing)
+Bottom_Two$Outfit_Reveal <- relevel(Bottom_Two$Outfit_Reveal, ref = "0")
 
-Lip_Sync_Ass <- glm(OUTCOME_LOSS ~ Lip_Sync_Ass, family=binomial(link='logit'), data=Bottom_Two)
-summary(Lip_Sync_Ass)
+Bottom_Two$Merle <- relevel(Bottom_Two$Merle, ref = "0")
 
-Expressed_Hardship <- glm(OUTCOME_LOSS ~ Expressed_Hardship, family=binomial(link='logit'), data=Bottom_Two)
-summary(Expressed_Hardship)
+Bottom_Two$Michelle <- relevel(Bottom_Two$Michelle, ref = "0")
+
+Bottom_Two$Ross <- relevel(Bottom_Two$Ross, ref = "0")
+
+Bottom_Two$Carson <- relevel(Bottom_Two$Carson, ref = "0")
+
+Bottom_Two$Do_they_know_words <- relevel(Bottom_Two$Do_They_Know_Words, ref = "0")
+
+Bottom_Two$Outfit_Reveal_1 <- relevel(Bottom_Two$Outfit_Reveal_1, ref = "0")
+
+Bottom_Two$Sewing <- relevel(Bottom_Two$Sewing, ref = "0")
+
+Bottom_Two$Dancing <- relevel(Bottom_Two$Dancing, ref = "0")
+
+Bottom_Two$Singing <- relevel(Bottom_Two$Singing, ref = "0")
+
+Bottom_Two$Lip_Sync_Ass <- relevel(Bottom_Two$Lip_Sync_Ass, ref = "0")
+
+Bottom_Two$Expressed_Hardship <- relevel(Bottom_Two$Expressed_Hardship, ref = "0")
+
+########################## UNIT BIVARIATE MODELS ###########################
+
+summary(Age <- glm(OUTCOME_LOSS ~ Age, family=binomial(link='logit'), data=Bottom_Two))
+
+summary(Hometown_City <- glm(OUTCOME_LOSS ~ Hometown_City, family=binomial(link='logit'), data=Bottom_Two))
+
+summary(Hometown_State <- glm(OUTCOME_LOSS ~ Hometown_State, family=binomial(link='logit'), data=Bottom_Two))
+
+summary(Wig_Removed <- glm(OUTCOME_LOSS ~ Wig_Removed, family=binomial(link='logit'), data=Bottom_Two))
+
+summary(Death_Drop <- glm(OUTCOME_LOSS ~ Death_Drop, family=binomial(link='logit'), data=Bottom_Two))
+
+summary(Outfit_Reveal <- glm(OUTCOME_LOSS ~ Outfit_Reveal, family=binomial(link='logit'), data=Bottom_Two))
+
+summary(Quality_Of_Outfit <- glm(OUTCOME_LOSS ~ Quality_Of_Outfit, family=binomial(link='logit'), data=Bottom_Two))
+
+summary(Merle <- glm(OUTCOME_LOSS ~ Merle, family=binomial(link='logit'), data=Bottom_Two))
+
+summary(Michelle <- glm(OUTCOME_LOSS ~ Michelle, family=binomial(link='logit'), data=Bottom_Two))
+
+summary(Santino <- glm(OUTCOME_LOSS ~ Santino, family=binomial(link='logit'), data=Bottom_Two))
+
+summary(Ross <- glm(OUTCOME_LOSS ~ Ross, family=binomial(link='logit'), data=Bottom_Two))
+
+summary(Carson <- glm(OUTCOME_LOSS ~ Carson, family=binomial(link='logit'), data=Bottom_Two))
+
+summary(Do_They_Know_Words <- glm(OUTCOME_LOSS ~ Do_They_Know_Words, family=binomial(link='logit'), data=Bottom_Two))
+
+summary(Outfit_Reveal_1 <- glm(OUTCOME_LOSS ~ Outfit_Reveal_1, family=binomial(link='logit'), data=Bottom_Two))
+
+summary(Gender <- glm(OUTCOME_LOSS ~ Gender, family=binomial(link='logit'), data=Bottom_Two))
+
+summary(Race <- glm(OUTCOME_LOSS ~ Race, family=binomial(link='logit'), data=Bottom_Two))
+
+summary(Body_Type <- glm(OUTCOME_LOSS ~ Body_Type, family=binomial(link='logit'), data=Bottom_Two))
+
+summary(Type_Queen <- glm(OUTCOME_LOSS ~ Type_Queen, family=binomial(link='logit'), data=Bottom_Two))
+
+summary(Sewing <- glm(OUTCOME_LOSS ~ Sewing, family=binomial(link='logit'), data=Bottom_Two))
+
+summary(Dancing <- glm(OUTCOME_LOSS ~ Dancing, family=binomial(link='logit'), data=Bottom_Two))
+
+summary(Singing <- glm(OUTCOME_LOSS ~ Singing, family=binomial(link='logit'), data=Bottom_Two))
+
+summary(Lip_Sync_Ass <- glm(OUTCOME_LOSS ~ Lip_Sync_Ass, family=binomial(link='logit'), data=Bottom_Two))
+
+summary(Expressed_Hardship <- glm(OUTCOME_LOSS ~ Expressed_Hardship, family=binomial(link='logit'), data=Bottom_Two))
 
